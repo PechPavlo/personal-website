b247216fb90015b47c9eddd87a248de09bb8dd09
PechPavlo <61708124+PechPavlo@users.noreply.github.com>
2020-06-01 20:06:03 +0300

    Updates

---
+++ precache-manifest.752ecaf45c252a4708f1cd00064db924.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "231a1efae46234b36492aa0d6133f12a",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "d773e56c9101930936e2",
+    "url": "/personal-website/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "48614f4fbfcc7bd6c24b",
+    "url": "/personal-website/static/js/2.ae021ef8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/personal-website/static/js/2.ae021ef8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d773e56c9101930936e2",
+    "url": "/personal-website/static/js/main.05b76dc2.chunk.js"
+  },
+  {
+    "revision": "8a0435702aa2b6325ff8",
+    "url": "/personal-website/static/js/runtime-main.d49f66af.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/personal-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
