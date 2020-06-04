a75fb18f8441785b1ad876757b59f83d683e472b
PechPavlo <61708124+PechPavlo@users.noreply.github.com>
2020-06-04 18:14:19 +0300

    some changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,53 @@
 import React from 'react';
-import logo from '../logo.svg';
+import pavlo1 from '../assets/img/pavlo1.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <p>
+          Hey, there! I'm Pavlo!<br/>
+          How's it going?
+          </p>
+        <img src={pavlo1} className="App-logo" alt="pavlo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Here will be my personal Website! (soon:-)
         </p>
+        <h3>Some my pages:</h3>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://pechpavlo.github.io/cappadocia/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn HTML & CSS by making Cappadocia page
         </a>
+        <a
+          className="App-link"
+          href="https://pechpavlo.github.io/HomeWork-9/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Homework in LemonSchool
+        </a>
+        <a
+          className="App-link"
+          href="http://PechPavlo.github.io/movie-posters"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learning React by making movie-poster page
+        </a>
+        <a
+          className="App-link"
+          href="http://PechPavlo.github.io/udemy-react"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          test page
+        </a>
+        <p>Keep calm, and learn React!!!</p>
       </header>
     </div>
   );
